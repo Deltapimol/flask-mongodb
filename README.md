fcfba256fb56fb1fa3170cf307993ec392b3bb77
Deltapimol <anmolshinde102@gmail.com>
2021-09-03 11:49:37 +0530

    Update README. Add delete and get methods for user collection. Other minor changes.

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Flask MongoDB POC
 
- 
+**API Endpoints**
+
+<ol>
+<li>Create user => localhost:5000/user</li>
+<ul>
+Request Body => 
+`{
+    "name": "Anmol Shinde",
+    "email": "anmolshinde106@gmail.com",
+    "password": "12345"
+}`
+</ul>
+<li>Get user => localhost:5000/user/<id></li>
+<li>List all users => localhost:5000/users</li>
+<li>Delete a user => localhost:5000/user/<id></li>
+</ol>
--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, json, jsonify, request, make_response
+from datetime import datetime
+
+from flask import Flask, json, jsonify, request
 
 from flask_pymongo import PyMongo
 import pymongo
 
-# from bson.json_util import dumps
-# from bson.objectid import ObjectId
+from pymongo.errors import ConnectionFailure, DuplicateKeyError, ServerSelectionTimeoutError, OperationFailure
+
+from bson.json_util import dumps
+from bson.objectid import ObjectId
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from config import MongoClass
+
+
 
 app = Flask(__name__)
 
@@ -21,45 +27,88 @@ app.config["MONGO_URI"] = f"mongodb+srv://{MONGO_CRED.MONGO_USER}:{MONGO_CRED.MO
 mongodb_client = PyMongo(app)
 
 
-@app.route('/add', methods=['POST'])
+@app.route('/user', methods=['POST'])
 def add_user():
-    print("REQUESTED API")
+    """
+    Add a new user to the collection
+    """
     _json = request.json
     _name = _json['name']
     _email = _json['email']
     _password = _json['password']
-    print("EMAIL", _email)
+    
     if _name and _email and _password and request.method == 'POST':
-        
         try:
-            # db_check = mongodb_client.db.user.find({"email":_email})
-            # print("DB CHECK", db_check)
-            # print("LIST", list(db_check))
-            # if db_check.count != 0:
-            #     return make_response(jsonify("User with this email already exists"), 400)
-            # else:
             _hashed_password = generate_password_hash(_password)
-            id = mongodb_client.db.users.insert({'name':_name, 'email':_email, 'password':_hashed_password})
-            # resp = jsonify(f"User '{_name}' created successfully!")
-            # resp.status_code = 200
-            
-            return make_response(jsonify(f"User '{_name}' created successfully!"), 200)  
-        except pymongo.errors.DuplicateKeyError:
-            return make_response(jsonify("User with this email address already exists"), 400)  
-        except pymongo.errors.ServerSelectionTimeoutError as ex:
-            return make_response(jsonify("Unable to connect to the MongoDB server"), 500)  
-        
+            id = mongodb_client.db.users.insert({'name':_name, 'email':_email, 'password':_hashed_password, 'create_date': datetime.now(), 'update_date': None})
+
+            return jsonify(f"User '{_name}' created successfully!"), 200
+        except DuplicateKeyError:
+            return email_already_exists()
+        except (ServerSelectionTimeoutError, ConnectionFailure):
+            return database_connection_error()
     else:
         return not_found()
 
-@app.errorhandler(404)
-def not_found(error=None):
-    message = 'Not Found' + request.url
-    resp = make_response(jsonify(message), 404)
+@app.route('/users', methods=['GET'])
+def list_users():
+    """
+    List all users from the user collection
+    """
+    try:
+        users = list(mongodb_client.db.users.find({} , {'password': False}))
+        return dumps(users), 200
+    except (ServerSelectionTimeoutError, ConnectionFailure):
+        return database_connection_error()
 
-    return resp
+@app.route('/user/<id>', methods=['GET'])
+def user(id):
+    """
+    Fetch specific user details
+    """
+    try:
+        user = mongodb_client.db.users.find_one({'_id':ObjectId(id)}, {'password': False})
+    except (ServerSelectionTimeoutError, ConnectionFailure):
+        return database_connection_error()
+    
+    if user is None:
+        return jsonify("User does not exist"), 404
+    return dumps(user), 200
+
+@app.route('/user/<id>', methods=['DELETE'])
+def delete(id):
+    """
+    Delete specific user details
+    """
+    try:
+        user = mongodb_client.db.users.delete_one({'_id':ObjectId(id)})
+    except (ServerSelectionTimeoutError, ConnectionFailure):
+        return database_connection_error()
+    
+    if user.deleted_count == 0:
+        return jsonify("User does not exist"), 404
+    return jsonify("User details deleted successfully"), 200
+
+@app.errorhandler(404)
+def not_found():
+    message = 'Not Found ' + request.url
+    return jsonify(message), 404
+
+@app.errorhandler(409)
+def email_already_exists():
+    message = 'Conflict. User with this email address already exists.'
+    return jsonify(message), 409
+
+@app.errorhandler(500)
+def database_connection_error():
+    message = 'Unable to connect to the MongoDB server'
+    return jsonify(message), 500
+          
                  
 if __name__ == "__main__":
     with app.app_context():
-        mongodb_client.db.users.create_index([("email", pymongo.ASCENDING),], unique=True)
+        try:
+            mongodb_client.db.users.create_index([("email", pymongo.ASCENDING),], unique=True)
+        except (ServerSelectionTimeoutError,ConnectionFailure) as ex:
+            print(ex)
     app.run(debug=True)
